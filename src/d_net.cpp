922d452290d7a2db71df9493b2b4fdc4cf403c7f
Edoardo Prezioso <edo88@email.it>
2016-02-16 20:01:12 +0100

    - Added optional 'z' parameter to the 'warp' CCMD.
    Now it's possible to warp above 3dfloors.

--- src/d_net.cpp
+++ src/d_net.cpp
@@ -2199,10 +2199,11 @@ void Net_DoCommand (int type, BYTE **stream, int player)
 
 	case DEM_WARPCHEAT:
 		{
-			int x, y;
+			int x, y, z;
 			x = ReadWord (stream);
 			y = ReadWord (stream);
-			P_TeleportMove (players[player].mo, x * 65536, y * 65536, ONFLOORZ, true);
+			z = ReadWord (stream);
+			P_TeleportMove (players[player].mo, x * 65536, y * 65536, z * 65536, true);
 		}
 		break;
 
