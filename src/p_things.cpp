966952d511f939e7bc70ce2b8a278aa9fa0c1b2a
MajorCooke <paul.growney22@gmail.com>
2016-07-22 17:58:34 -0500

    More thoroughly initiate positions, as the testing flag or failure can cause actors to fall as well.

--- src/p_things.cpp
+++ src/p_things.cpp
@@ -875,11 +875,13 @@ int P_Thing_Warp(AActor *caller, AActor *reference, double xofs, double yofs, do
 		}
 	}
 
+	P_TryMove(caller, caller->Pos(), false);
 	if ((flags & WARPF_NOCHECKPOSITION) || P_TestMobjLocation(caller))
 	{
 		if (flags & WARPF_TESTONLY)
 		{
 			caller->SetOrigin(old, true);
+			P_TryMove(caller, caller->Pos(), false);
 		}
 		else
 		{
@@ -925,12 +927,10 @@ int P_Thing_Warp(AActor *caller, AActor *reference, double xofs, double yofs, do
 			{
 				caller->AddZ(reference->GetBobOffset());
 			}
-
-			DVector2 pos = caller->Pos();
-			P_TryMove(caller, pos, false);
 		}
 		return true;
 	}
 	caller->SetOrigin(old, true);
+	P_TryMove(caller, caller->Pos(), false);
 	return false;
 }
